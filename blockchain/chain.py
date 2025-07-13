06cff6164c0e6253ce4cb2787fce128e39fdef65
ahmed kamel <aa.ak73@gmail.com>
2025-07-13 15:20:42 +0300

    Update chain.py

--- blockchain/chain.py
+++ blockchain/chain.py
@@ -1,163 +1,78 @@
 # blockchain/chain.py
 
-
-
 import time
-
-from .block import Block, create_genesis_block
-
+import hashlib
+from .block import Block
 from .transaction import Transaction
 
-from .config import BLOCK_TIME_SECONDS, ADMIN_ADDRESS, TOTAL_SUPPLY
-
-from .reward import is_eligible_for_reward, get_daily_reward_transaction
-
-from .referral import get_referral_bonus_transaction
-
-
-
-
-
 class Blockchain:
-
     def __init__(self):
-
-        self.chain = [create_genesis_block()]
-
+        self.chain = []
         self.pending_transactions = []
+        self.users = {}  # قاعدة بيانات مؤقتة للمستخدمين
+        self.load_genesis_block()
 
-        self.balances = {ADMIN_ADDRESS: TOTAL_SUPPLY}
+    def load_genesis_block(self):
+        if not self.chain:
+            genesis_block = Block(0, "0", [], time.time())
+            self.chain.append(genesis_block)
 
-        self.last_mining_times = {}
+    def add_transaction(self, transaction: Transaction):
+        is_valid, message = Transaction.is_valid(transaction, self.users)
 
+        if not is_valid:
+            return False, message
 
+        sender = transaction.sender
+        recipient = transaction.recipient
+        amount = transaction.amount
 
-    def get_last_block(self):
+        # خصم وإضافة الأرصدة
+        self.users[sender]["balance"] -= amount
+        self.users[recipient]["balance"] += amount
 
-        return self.chain[-1]
+        self.pending_transactions.append(transaction)
+        return True, "Transaction added successfully"
 
+    def mine_block(self, miner_address):
+        if not self.pending_transactions:
+            return False, "No transactions to mine"
 
+        if miner_address not in self.users or not self.users[miner_address]["kyc_passed"]:
+            return False, "Miner must be a verified user"
 
-    def add_transaction(self, tx):
+        # مكافأة التعدين
+        reward_amount = 3
+        self.users[miner_address]["balance"] += reward_amount
 
-        if tx.is_valid():
-
-            self.pending_transactions.append(tx)
-
-            return True
-
-        return False
-
-
-
-    def get_balance(self, address):
-
-        return self.balances.get(address, 0)
-
-
-
-    def is_valid_chain(self):
-
-        for i in range(1, len(self.chain)):
-
-            current = self.chain[i]
-
-            previous = self.chain[i - 1]
-
-            if current.previous_hash != previous.hash:
-
-                return False
-
-            if current.hash != current.calculate_hash():
-
-                return False
-
-        return True
-
-
-
-    def mine_pending_transactions(self, miner_address, cycle_count, referrer=None):
-
-        now = int(time.time())
-
-        last_mined = self.last_mining_times.get(miner_address, 0)
-
-
-
-        if now - last_mined < BLOCK_TIME_SECONDS:
-
-            return None  # لم يمر وقت كافي للتعدين
-
-
-
-        reward_tx = get_daily_reward_transaction(miner_address)
-
-        transactions = [reward_tx]
-
-
-
-        if referrer and cycle_count >= 30:
-
-            ref_tx = get_referral_bonus_transaction(referrer, miner_address)
-
-            if ref_tx:
-
-                transactions.append(ref_tx)
-
-
-
-        transactions.extend(self.pending_transactions)
-
-
-
+        last_block = self.chain[-1]
         new_block = Block(
-
-            index=len(self.chain),
-
-            previous_hash=self.get_last_block().hash,
-
-            timestamp=now,
-
-            transactions=[tx.to_dict() for tx in transactions],
-
-            creator_address=miner_address
-
+            index=last_block.index + 1,
+            previous_hash=last_block.hash,
+            transactions=self.pending_transactions.copy(),
+            timestamp=time.time()
         )
 
+        new_block.hash = new_block.compute_hash()
         self.chain.append(new_block)
-
-
-
-        for tx in transactions:
-
-            self._update_balances(tx)
-
-
-
         self.pending_transactions = []
+        return True, "Block mined and added to the chain"
 
-        self.last_mining_times[miner_address] = now
+    def get_balance(self, address):
+        user = self.users.get(address)
+        if not user:
+            return 0
+        return user.get("balance", 0)
 
+    def register_user(self, address):
+        if address not in self.users:
+            self.users[address] = {
+                "balance": 0,
+                "kyc_passed": False,
+                "referrals": 0,
+                "rank": "member"
+            }
 
-
-        return new_block
-
-
-
-    def _update_balances(self, tx):
-
-        sender = tx["sender"]
-
-        recipient = tx["recipient"]
-
-        amount = tx["amount"]
-
-
-
-        if sender != "COINBASE":
-
-            self.balances[sender] = self.balances.get(sender, 0) - amount
-
-
-
-        self.balances[recipient] = self.balances.get(recipient, 0) + amount
+    def verify_kyc(self, address):
+        if address in self.users:
+            self.users[address]["kyc_passed"] = True
