df67557f54e9ba0d67b2e8f522e6c731a55eed20
ahmed kamel <aa.ak73@gmail.com>
2025-07-15 23:30:47 +0300

    Update kyc.py

--- blockchain/kyc.py
+++ blockchain/kyc.py
@@ -1,21 +1,24 @@
 # blockchain/kyc.py
 
-
-
 class KYCRegistry:
-
     def __init__(self):
-
         self.verified_users = set()
 
-
-
     def verify_user(self, address):
-
         self.verified_users.add(address)
 
-
-
     def is_verified(self, address):
-
         return address in self.verified_users
+
+
+# دالة وهمية لمعالجة مستند KYC
+def process_kyc_document(document):
+    # في النظام الفعلي: تحليل المستندات والتحقق منها
+    return True
+
+
+# دالة للتحقق مما إذا كان المستخدم موثّق
+def is_user_verified(address, kyc_registry=None):
+    if kyc_registry:
+        return kyc_registry.is_verified(address)
+    return False
