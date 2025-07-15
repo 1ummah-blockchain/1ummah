fcdb2f89bd64f4370be5b156b0171910a71cdc53
ahmed kamel <aa.ak73@gmail.com>
2025-07-16 00:10:29 +0300

    Update kyc.py

--- blockchain/kyc.py
+++ blockchain/kyc.py
@@ -4,20 +4,20 @@ class KYCRegistry:
     def __init__(self):
         self.verified_users = set()
 
-    def verify_user(self, address_or_email):
-        self.verified_users.add(address_or_email)
+    def verify_user(self, email):
+        self.verified_users.add(email)
 
-    def is_verified(self, address_or_email):
-        return address_or_email in self.verified_users
+    def is_verified(self, email):
+        return email in self.verified_users
 
 
-# سجل واحد مشترك
+# كائن ثابت لاستخدامه في الراوتر
 kyc_registry = KYCRegistry()
 
 
 def process_kyc_document(email, document_path, selfie_path):
     """
-    محاكاة عملية تحقق KYC ناجحة.
+    إجراء تحقق وهمي للوثائق - يعتبر دائماً ناجح.
     """
     kyc_registry.verify_user(email)
     return True
