4b55fa9352c9b7f9c6dcc3685ab666d10f7c85aa
ahmed kamel <aa.ak73@gmail.com>
2025-07-17 13:59:22 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# تحميل بيانات الحساب من المسار الصحيح
+cred = credentials.Certificate('config/serviceAccountKey.json')
+
+# تهيئة Firebase
 firebase_admin.initialize_app(cred)
+
+# تهيئة قاعدة بيانات Firestore
 db = firestore.client()
